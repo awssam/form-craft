f19d0fddc513f1b622e61fee7d3e5edccc453f2d
Varadarajan M <varad2k12@gmail.com>
2025-01-06 14:08:31 +0530

    refactor: added skeleton for username when loading

--- src/app/(dashboard)/_components/TabsContainer.tsx
+++ src/app/(dashboard)/_components/TabsContainer.tsx
@@ -1,18 +1,15 @@
 'use client';
 
+import { Skeleton } from '@/components/ui/skeleton';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { useUser } from '@clerk/nextjs';
 import { usePathname, useRouter } from 'next/navigation';
 import React from 'react';
 
 const TabsContainer = ({ children }: { children: React.ReactNode }) => {
-  const { isLoaded, isSignedIn, user } = useUser();
+  const { isLoaded, user } = useUser();
   const pathname = usePathname();
   const router = useRouter();
-
-  if (!isLoaded || !isSignedIn) {
-    return null;
-  }
 
   return (
     <Tabs
@@ -25,10 +22,19 @@ const TabsContainer = ({ children }: { children: React.ReactNode }) => {
     >
       <div className="flex justify-between gap-6 md:items-center sm:flex-row flex-col">
         <div className="div text-center md:text-left">
-          <h2 className="font-bold text-white md:text-2xl text-lg">Hey {user?.firstName}!! 👋</h2>
-          <p className="text-xs text-muted-foreground">
-            This is your dashboard, here you can see how your forms are performing, recent activity and more.
-          </p>
+          {!isLoaded ? (
+            <Skeleton className="h-7 w-52 rounded-md mx-auto sm:mx-0" />
+          ) : (
+            <h2 className="font-bold text-white md:text-2xl text-lg">Hey {user?.firstName ?? '...'} 👋</h2>
+          )}
+
+          {!isLoaded ? (
+            <Skeleton className="h-3 w-96 rounded-md mt-1 mx-auto sm:mx-0" />
+          ) : (
+            <p className="text-xs text-muted-foreground">
+              This is your dashboard, here you can see how your forms are performing, recent activity and more.
+            </p>
+          )}
         </div>
         <div className="justify-between gap-3 items-center sm:flex mx-auto sm:m-0">
           {/* <DateTimePicker className="md:w-[200px]" placeHolderClasses="text-white" granularity="day" /> */}
