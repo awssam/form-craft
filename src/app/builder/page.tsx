a8f8e60a419627f9a2cbfaa380634e008c8c07ad
Varadarajan M <varad2k12@gmail.com>
2025-03-16 19:31:00 +0530

    builder revalidation time change

--- src/app/builder/page.tsx
+++ src/app/builder/page.tsx
@@ -2,7 +2,7 @@ import TopHeader from '@/components/common/TopHeader';
 import SectionDisplay from '@/app/builder/_components/SectionDisplay';
 import { FormSectionDisplayProvider } from '@/hooks/useFormSectionDisplay';
 
-export const revalidate = false;
+export const revalidate = Infinity;
 
 const FormBuilderPage = async () => {
   return (
